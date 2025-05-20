5977cfccfc8a86e848dcf7badbb7e9d4a675973e
Kirb <jenshobo@gmail.com>
2025-05-20 13:53:50 +0200

    feat: add basic commands
    adding basic commands for starting up, sitting, speaking and changing the LED color

--- example/g1/CoP/g1_cop_event.cpp
+++ example/g1/CoP/g1_cop_event.cpp
@@ -1,5 +1,122 @@
 #include <iostream>
+#include <string>
+
+#include <unitree/robot/channel/channel_subscriber.hpp>
+#include <unitree/robot/g1/loco/g1_loco_client.hpp>
+#include <unitree/robot/g1/audio/g1_audio_client.hpp>
+
+enum fsm_states {
+    ZERO_TORQUE         = 0,
+    DAMPING             = 1,
+    SQUAD               = 2,
+    SIT                 = 3,
+    LOCK_STAND          = 4,
+    MAIN_MOTION_CONTROL = 200
+};
+
+unitree::robot::g1::LocoClient setupLocoClient() {
+    unitree::robot::g1::LocoClient client;
+
+    client.Init();
+    client.SetTimeout(10.f);
+
+    return client;
+}
+
+unitree::robot::g1::AudioClient setupAudioClient() {
+    unitree::robot::g1::AudioClient client;
+
+    client.Init();
+    client.SetTimeout(10.f);
+
+    return client;
+}
+
+void start() {
+    unitree::robot::g1::LocoClient client = setupLocoClient();
+
+    fsm_states state;
+    state = DAMPING;
+    client.SetFsmId(state);
+    unitree::common::Sleep(1);
+    state = LOCK_STAND;
+    client.SetFsmId(state);
+    unitree::common::Sleep(3);
+    state = MAIN_MOTION_CONTROL;
+    client.SetFsmId(state);
+}
+
+void sit() {
+    unitree::robot::g1::LocoClient client = setupLocoClient();
+
+    fsm_states state;
+    state = SIT;
+    client.SetFsmId(state);
+    unitree::common::Sleep(3);
+    state = DAMPING;
+    client.SetFsmId(state);
+}
+
+void say() {
+    unitree::robot::g1::AudioClient client = setupAudioClient();
+
+    std::string in;
+    std::getline(std::cin, in);
+
+    int32_t ret;
+    ret = client.TtsMaker(in, 1);
+}
+
+void eyecolor() {
+    unitree::robot::g1::AudioClient client = setupAudioClient();
+
+    std::string in;
+    std::getline(std::cin, in);
+
+    uint8_t r, g, b;
+    std::stringstream stream;
+
+    stream << std::hex << in.substr(0, 2);
+    stream >> r;
+    stream.clear();
+
+    stream << std::hex << in.substr(2, 4);
+    stream >> g;
+    stream.clear();
+
+    stream << std::hex << in.substr(4, 6);
+    stream >> b;
+    stream.clear();
+
+    client.LedControl(r, g, b);
+}
 
 int main(int argc, char const *argv[]) {
-    std::cout << "Hallo World!" << std::endl;
+    if (argc < 2) {
+        std::cout << "Usage: audio_client_example [NetWorkInterface(eth0)]" << std::endl;
+        exit(0);
+    }
+
+    std::unordered_map<std::string, void(*)()> functionMap = {
+        {"--start", &start},
+        {"--sit", &sit},
+        {"--say", &say},
+        {"--eyecolor", &eyecolor}
+    };
+
+    unitree::robot::ChannelFactory::Instance()->Init(0, argv[1]);
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+
+        auto it = functionMap.find(arg);
+        if (it != functionMap.end()) {
+            it->second();
+        } else {
+            std::cerr << "Error: Function '" << arg << "' does not exist." << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
 }
